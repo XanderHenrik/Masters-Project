70eac4b0668a0870ac7deb4a1c1c71c522ae98ba
Xander Noordeloos <xandernoordeloos@Xanders-MacBook-Air.local>
2026-01-02 11:26:57 +0100

    Small adjustments, inverted the plotting order sucht that the equality scale factors are plotted in the background and corrected the name of the saved file

--- rho_scaling_figure.py
+++ rho_scaling_figure.py
@@ -17,24 +17,26 @@ rho_de = (np.ones_like(a) * 0.68)
 
 # Create the figure:
 plt.figure()
-# plt.subplots_adjust(bottom=0.5)
+plt.subplots_adjust(left=0.15)
 # plt.loglog(a, (rho_m * 0.3166), color='g', label=r"$\rho_{m}$")
 # plt.loglog(a, (rho_r * 9.4e-5), color='r', label=r"$\rho_{r}$")
 # plt.loglog(a, (rho_de), color='b', label=r"$\rho_{\Lambda}$")
+
+# equality scalefactors (determined by eye-balling)
+plt.axvline(x=2.97e-4, linestyle=':', color='k', alpha=0.5, label=r"$a_{eq}$")
+plt.axvline(x=7.75e-1, linestyle='--', color='k', alpha=0.5, label=r"$a_{\Lambda}$")
 
 # Or in the color theme of the Neutrino hierarchy plot:
 plt.loglog(a, (rho_m * 0.3166), color='royalblue', label=r"$\rho_{m}$")
 plt.loglog(a, (rho_r * 9.4e-5), color='orangered', label=r"$\rho_{r}$")
 plt.loglog(a, (rho_de), color='gold', label=r"$\rho_{\Lambda}$")
 
-# equality scalefactors (determined by eye-balling)
-plt.axvline(x=3.05e-4, linestyle=':', color='k', alpha=0.5, label=r"$a_{eq}$")
-plt.axvline(x=7.65e-1, linestyle='--', color='k', alpha=0.5, label=r"$a_{\Lambda}$")
 
 # plt.xscale('log')
 plt.ylim(1e-2, 1e12)
-plt.xlabel(r"$a(t)$")
-plt.ylabel(r"log$\left[\frac{\rho(t)}{\rho_{crit, 0}}\right]$", rotation=90)
+plt.xlabel(r"$a(t)$", fontsize=14)
+plt.ylabel(r"$\frac{\rho(t)}{\rho_{crit, 0}}$", fontsize=16)
 plt.grid(which='both', alpha=0.25)
-plt.legend()
+plt.legend(fontsize=12)
+plt.savefig("rho_scaling_expansion_history.pdf")
 plt.show()
